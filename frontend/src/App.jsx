b37d06985e11aa529423e81de119d840b4cc7e29
Saroj Prasad Mainali <sarojprasadmainali@gmail.com>
2023-09-04 18:58:23 +0545

    Started fashion-kathmandu frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-green-200">App</h1>
+      </div>
+      <main>List Product</main>
+    </>
+  );
+}
+
+export default App;
