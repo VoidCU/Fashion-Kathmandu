0e071067456481ce41ea9df0e2a3e7146f0201b8
Saroj Prasad Mainali <sarojprasadmainali@gmail.com>
2023-09-05 23:03:19 +0545

    About Us page added and removed bug

---
+++ frontend/src/pages/About/About.jsx
@@ -0,0 +1,44 @@
+function About() {
+  return (
+    <>
+      <div className="flex-col max-w-6xl p-4 m-auto">
+        <div className="w-full m-auto">
+          <img className="m-auto" src="images/workers.jpg" alt="" />
+        </div>
+        <div className="text-center font-bold text-2xl p-4">
+          Welcome to Fashion Kathmandu
+        </div>
+        <div className="text-justify">
+          <span className="font-bold">Fashion Kathmandu</span> is one of the
+          largest leading Nepalese handmade women and men&apos;s clothing,
+          handicraft, and accessories manufacturer, wholesaler, and exporter
+          company in Kathmandu, Nepal.{' '}
+          <span className="font-bold">Fashion Kathmandu</span> might want to
+          thank you for choosing our company as one of your suppliers, and
+          promoting our items from the beautiful country Nepal.{' '}
+          <span className="font-bold">Fashion Kathmandu</span> is a Nepal-based
+          company making an honest effort to make a brand name on Nepalese hand
+          tailored dress. This company has flourished the traditional arts and
+          creativity on clothing focusing on a new era of fashion. It has
+          likewise promoted the job opportunities of Nepalese women giving hand
+          works at home and supporting the local community for their financial
+          growth. <br /> Nepalese handmade clothing, handicrafts, and
+          accessories not only have historical and cultural significance, but
+          also economic viability. We are putting our best efforts for
+          introducing Nepalese handmade products to the international market.{' '}
+          <span className="font-bold">Fashion Kathmandu</span> Craft, an
+          umbrella organization, is the result of this attempt. We believe that
+          the Nepalese handmade clothing and handicrafts owe its genesis to us.
+          We have the vision of expanding Nepalese handmade products with fair
+          trade ethics in the international market and popularizing them.{' '}
+          <span className="font-bold">Fashion Kathmandu</span> Craft offers the
+          immense variety of clothing with good quality. Furthermore, the
+          company always pays attention to the customers&apos; best satisfaction
+          and continued relation for promoting the mutual business.
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
