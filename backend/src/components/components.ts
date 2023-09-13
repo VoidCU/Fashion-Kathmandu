8155f3d5292a6d493ea975e100fce5e4a400b408
Saroj Prasad Mainali <sarojprasadmainali@gmail.com>
2023-09-13 13:52:40 +0545

    git push silly mistake

--- backend/src/components/components.ts
+++ backend/src/components/components.ts
@@ -1,10 +1,5 @@
 import { ComponentLoader } from 'adminjs';
 const componentLoader = new ComponentLoader();
 
-const Components = {
-  myImage: componentLoader.add(
-    'myImage',
-    'C:/Users/VoidCU/Desktop/backend/dist/components/Image'
-  ),
-};
+const Components = {};
 export { componentLoader, Components };
