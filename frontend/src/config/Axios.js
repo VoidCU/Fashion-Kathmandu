756d2a84ab688773a871e7ff3846652f5c53ebe6
Saroj Prasad Mainali <sarojprasadmainali@gmail.com>
2023-09-13 14:23:10 +0545

    Added base url

--- frontend/src/config/Axios.js
+++ frontend/src/config/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://141.148.206.219:3000',
   headers: {
     'Content-Type': 'application/json',
     accept: 'application/json',
