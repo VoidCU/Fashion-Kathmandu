c05673eef0326f0ae91b88c81c3c6da97d9eecef
Saroj Prasad Mainali <sarojprasadmainali@gmail.com>
2023-09-18 22:07:20 +0545

    Changed Helmet locations

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ProductsProvider } from './context/ProductsContext.jsx';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
 
 document.cookie = '_ga=value; SameSite=None; Secure';
 document.cookie = '_ga_21SLH4J369=value; SameSite=None; Secure';
@@ -12,24 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <ProductsProvider>
-        <App>
-          <Helmet>
-            <title>Fashion Kathmandu</title>
-            <meta
-              name="description"
-              content="Nepal-based company making an honest effort to make a brand name on Nepalese hand tailored dress"
-            />
-            <meta property="og:image" content="images/webLogo.png" />
-            <meta property="og:title" content="Fashion Kathmandu" />
-            <meta
-              property="og:description"
-              content="Nepal-based company making an honest effort to make a brand name on Nepalese hand tailored dress"
-            />
-            <meta property="og:type" content="website" />
-            <meta property="og:site_name" content="Fashion Kathmandu" />
-            <meta name="twitter:image" content="images/webLogo.png" />
-          </Helmet>
-        </App>
+        <App />
       </ProductsProvider>
     </HelmetProvider>
   </React.StrictMode>
--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -3,6 +3,7 @@ import Benefits from './Benefits';
 import Topitems from './Topitems';
 import Newitems from './Newitems';
 import Category from './Category';
+import { Helmet } from 'react-helmet-async';
 
 import { useProducts } from '../../context/ProductsContext';
 
@@ -22,6 +23,22 @@ function Homepage() {
   const newestItems = filteredProducts.slice(0, 6);
   return (
     <>
+      <Helmet>
+        <title>Fashion Kathmandu</title>
+        <meta
+          name="description"
+          content="Nepal-based company making an honest effort to make a brand name on Nepalese hand tailored dress"
+        />
+        <meta property="og:image" content="images/webLogo.png" />
+        <meta property="og:title" content="Fashion Kathmandu" />
+        <meta
+          property="og:description"
+          content="Nepal-based company making an honest effort to make a brand name on Nepalese hand tailored dress"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Fashion Kathmandu" />
+        <meta name="twitter:image" content="images/webLogo.png" />
+      </Helmet>
       {products.length > 0 && <Hero featured={featured} />}
       <Benefits />
       <Topitems topitems={randomTopItems} />
